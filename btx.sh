05cd807106b0c7409e46d08f3ee80e713912288e
MasterBrew <mantronix@email.com>
2019-06-20 02:21:18 +0200

    Bitcore [BTX]

--- btx.sh
+++ btx.sh
@@ -31,12 +31,12 @@ printf "\t\t\t "
 for (( counter=10; counter>=0; counter-- ))
 do
 echo -n "$counter "
-sleep .01
+sleep .1
 done
 printf "\n\n"
 
 # Sleep for .5 seconds
-sleep .05
+sleep .5
 
 # Logging fase 5 for later analizing comping time and state..   
 var1=`date`
@@ -47,11 +47,11 @@ sudo /etc/init.d/dphys-swapfile start
 printf "\n"
 # Show free memory
 free
-printf "\n${Red}Check the swapfile= 1024!, If not okey use 'sudo nano /etc/dphys-swapfile'${NC}\n"
+printf "\n${Red}Check  swapfile=1024 if not okey?!  use 'sudo nano /etc/dphys-swapfile'${NC}\n"
 
 sleep 1
-#sudo nano /etc/dphys-swapfile
-
+# Use this to change the size of swapfile if not correct!
+# sudo nano /etc/dphys-swapfile
 
 var2=`date`
 echo "Start fase 5 : $var2 - Installing Git  " >> fase5.log
@@ -61,7 +61,7 @@ printf "\n"
 printf "${Purple}###############################################################################${NC}\n"
 printf "${BWhite}  \t\t\t\t $NEXT_STEP     ${NC}\n"
 printf "${Purple}###############################################################################${NC}\n\n"
-#sudo apt-get install git
+sudo apt-get install git
 
 var3=`date`
 echo "Start fase 5 : $var3 - Making ~/wallets directory" >> fase5.log
@@ -97,22 +97,58 @@ git clone https://github.com/LIMXTEC/BitCore
 cd BitCore
 printf "\n"
 ls -l
-sleep 1
+sleep 5
 
-# printf "\n"
-# read -n 1 -s -r -p "           * Press key to start frontend parsing BitCore  *"
-# printf "\n"
+var5=`date`
+echo "Start fase 5 : $var5 - Start ./autogen.sh" >> fase5.log
 
-
-NEXT_STEP="Please Wait Wile ./autogen.sh is running "
+NEXT_STEP="Please wait ...  ./autogen.sh is running "
 printf "\n"
 printf "${Purple}###############################################################################${NC}\n"
 printf "${White}  \t\t $NEXT_STEP     ${NC}\n"
 printf "${Purple}###############################################################################${NC}\n\n"
 sleep 2
 printf "meditate while it takes some time......\n"
+./autogen.sh
 
+var6=`date`
+echo "Start fase 5 : $var6 - Start ./configuration" >> fase5.log
 
+NEXT_STEP="Starting ./configuration and its Flaggs!!"
+printf "\n"
+printf "${Purple}###############################################################################${NC}\n"
+printf "${White}  \t\t $NEXT_STEP     ${NC}\n"
+printf "${Purple}###############################################################################${NC}\n\n"
+sleep 2
+./configure  CXXFLAGS="--param ggc-min-expand=1 --param ggc-min-heapsize=32768" CPPFLAGS="-I/usr/local/BerkeleyDB.4.8/include -O" LDFLAGS="-L/usr/local/BerkeleyDB.4.8/lib" --disable-tests --disable-bench --enable-upnp-default --with-gui --enable-glibc-back-compat --enable-reduce-exports
+
+var7=`date`
+echo "Start fase 5 : $var7 - Start make -j2" >> fase5.log
+
+NEXT_STEP="make -j2 (using 2 proccessors)"
+printf "\n"
+printf "${Purple}###############################################################################${NC}\n"
+printf "${White}  \t\t $NEXT_STEP     ${NC}\n"
+printf "${Purple}###############################################################################${NC}\n\n"
+sleep 2
+make -j2
+
+var8=`date`
+echo "Start fase 5 : $var8 - sudo make install" >> fase5.log
+
+NEXT_STEP="Sudo make install => Create the binairies!! SRC "
+printf "\n"
+printf "${Purple}###############################################################################${NC}\n"
+printf "${White}  \t\t $NEXT_STEP     ${NC}\n"
+printf "${Purple}###############################################################################${NC}\n\n"
+sleep 2
+sudo make install
+
+var9=`date`
+echo "Start fase 5 : $var9 - Done..." >> fase5.log
+echo ''
+
+cat fase5.log
 
 # It ends
 printf "\n[END]\n"
